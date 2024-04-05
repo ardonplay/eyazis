6a1604ac4bf848de54f68c6117f8412a5537a181
ardonplay <ardonplay@gmail.com>
2024-04-05 13:21:45 +0300

    dev: update env variables

--- LR2/server/server/main.py
+++ LR2/server/server/main.py
@@ -3,7 +3,7 @@ import uvicorn
 
 
 def start():
-    port = int(os.getenv("SERVER_PORT", 8000))
-    host = os.getenv("SERVER_HOST", "0.0.0.0")
+    port = int(os.getenv("LR2_SERVER_PORT", 8000))
+    host = os.getenv("LR2_SERVER_HOST", "0.0.0.0")
 
-    uvicorn.run("server.server:app", host=host, port=port, reload=True)
+    uvicorn.run("server.sersver:app", host=host, port=port, reload=True)
