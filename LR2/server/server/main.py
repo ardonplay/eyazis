a7da8f408172155da24613b2020078455063ddff
ardonplay <ardonplay@gmail.com>
2024-04-05 14:07:21 +0300

    fix: change python version

--- LR2/server/server/main.py
+++ LR2/server/server/main.py
@@ -6,4 +6,4 @@ def start():
     port = int(os.getenv("LR2_SERVER_PORT", 8000))
     host = os.getenv("LR2_SERVER_HOST", "0.0.0.0")
 
-    uvicorn.run("server.sersver:app", host=host, port=port, reload=True)
+    uvicorn.run("server.server:app", host=host, port=port, reload=True)
