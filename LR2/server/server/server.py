352d689844f3001b228d207c7102e9fd5ee13e2a
ardonplay <ardonplay@gmail.com>
2024-04-05 14:14:38 +0300

    feat: cors

--- LR2/server/server/server.py
+++ LR2/server/server/server.py
@@ -1,11 +1,25 @@
 from typing import Annotated
 from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
+
 from server.utils import basis
 
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight": True})
 
+origins = [
+ "*"
+]
 
-@app.get("/api/v1/lr2")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/api/v1/lr2")
 async def root(text: Annotated[str, Form()]):
     return basis.tokenize(text)
 
